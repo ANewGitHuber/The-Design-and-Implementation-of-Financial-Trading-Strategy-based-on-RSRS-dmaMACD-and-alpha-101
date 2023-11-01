9c209ed879bc1a14932bc3148fbd6e18d68419e7
John <88078123+ANewGitHuber@users.noreply.github.com>
2023-11-01 17:31:39 +0000

    Add files via upload

---
+++ Position Visualization/k_plot.R
@@ -0,0 +1,17 @@
+library(quantmod)
+
+rm(list = ls())
+setwd("/Users/zyx/Desktop/backtester_v5.7_final/DATA/PART1")
+price <- as.xts(read.zoo("10.csv",header=TRUE,sep=",",colClasses = c("Date", rep("numeric",5))))
+
+n <- nrow(price)
+m <- nrow(price)-1000
+
+#pdf(file = "k.pdf")
+chartSeries(price[c(m:n)],theme = chartTheme("white"),
+            up.col = "red",dn.col = "green",name = "series_07",
+            time.scale = 0.5,line.type = "l",bar.type = "ohlc",
+            major.ticks='auto', minor.ticks=TRUE)
+#dev.off()
+chartSeries(price[c(m:n)],theme = chartTheme("white"),TA = "addSMA(n=10);addSMA(n=80);addMACD(type = 'EMA')")
+
